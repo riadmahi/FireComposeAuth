3995934046031ca97c6262347450d6aab84cc723
Riad Mahi <riad.mahi@remedee.com>
2025-05-22 15:34:27 +0200

    Add simple firebase auth wrapper interface

---
+++ firecomposeauth/src/commonMain/kotlin/com/riadmahi/firecomposeauth/FireComposeAuth.kt
@@ -0,0 +1,8 @@
+package com.riadmahi.firecomposeauth
+
+interface FireComposeAuth {
+    suspend fun login(email: String, password: String): AuthResult
+    suspend fun register(email: String, password: String): AuthResult
+    suspend fun logout()
+    fun currentUser(): AuthUser?
+}
