dc8e7c9710fd076fdc3e076bb880b7572a3b2280
Riad Mahi <riad.mahi@remedee.com>
2025-05-23 16:16:14 +0200

    Update auth result

--- firecomposeauth/src/commonMain/kotlin/com/riadmahi/firecomposeauth/AuthResult.kt
+++ firecomposeauth/src/commonMain/kotlin/com/riadmahi/firecomposeauth/AuthResult.kt
@@ -1,6 +1,6 @@
 package com.riadmahi.firecomposeauth
 
 sealed interface AuthResult {
-    data class Success(val user: AuthUser) : AuthResult
+    data object Success : AuthResult
     data class Error(val message: String) : AuthResult
 }
