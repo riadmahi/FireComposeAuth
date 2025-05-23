8499356285c9259db3e971b7e3414e6ab65e55ff
Riad Mahi <riad.mahi@remedee.com>
2025-05-23 17:05:06 +0200

    Add all Firebase Auth Error code for wrapping

---
+++ firecomposeauth/src/commonMain/kotlin/com/riadmahi/firecomposeauth/FirebaseAuthErrorCodes.kt
@@ -0,0 +1,11 @@
+package com.riadmahi.firecomposeauth
+
+object FirebaseAuthErrorCodes {
+    const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
+    const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
+    const val ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND"
+    const val ERROR_TOO_MANY_REQUESTS = "ERROR_TOO_MANY_REQUESTS"
+    const val ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE"
+    const val ERROR_WEAK_PASSWORD = "ERROR_WEAK_PASSWORD"
+    const val UNKNOWN = "ERROR_UNKNOWN"
+}
