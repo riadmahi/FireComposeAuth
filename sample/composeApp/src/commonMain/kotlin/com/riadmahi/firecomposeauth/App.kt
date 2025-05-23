e86ce2205cd6e4b0920f8256fcebe399dd935cea
Riad Mahi <riad.mahi@remedee.com>
2025-05-23 14:36:54 +0200

    Add simple implementation of login

--- sample/composeApp/src/commonMain/kotlin/com/riadmahi/firecomposeauth/App.kt
+++ sample/composeApp/src/commonMain/kotlin/com/riadmahi/firecomposeauth/App.kt
@@ -1,43 +1,65 @@
-package com.riadmahi.firecomposeauth
+package com.riadmahi.sample
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import firecomposeauth.sample.composeapp.generated.resources.Res
-import firecomposeauth.sample.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import com.riadmahi.firecomposeauth.AuthResult
+import com.riadmahi.firecomposeauth.getFireComposeAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val context = getPlatformContext()
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf<String?>(null) }
+    val auth = getFireComposeAuth(getPlatformContext())
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Column(
+                modifier = Modifier
+                    .safeContentPadding()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TextField(value = email, onValueChange = { email = it }, label = { Text("Email") })
+                TextField(value = password, onValueChange = { password = it }, label = { Text("Password") })
+
+                Button(onClick = {
+                    result = null
+                    CoroutineScope(Dispatchers.Main).launch {
+                        try {
+                            val resultValue = auth.login(email, password)
+                            result = when (resultValue) {
+                                is AuthResult.Success -> "✅ Welcome ${resultValue.user.email}"
+                                is AuthResult.Error -> "❌ ${resultValue.message}"
+                            }
+                        } catch (e: Exception) {
+                            result = "❌ ${e.message}"
+                        }
+                    }
+                }) {
+                    Text("Login")
+                }
+
+                result?.let {
+                    Text(it)
                 }
             }
-        }
     }
 }
